5c25275ef8178a765e4c6de67bed028ed1f7d323
krishnayoga <ida.bagus52@ui.ac.id>
2018-08-09 10:57:01 +0700

    first commit

---
+++ AI_StreamingAI/StreamingAIForm.Designer.cs
@@ -0,0 +1,169 @@
+namespace AI_StreamingAI
+{
+   partial class StreamingAIForm
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StreamingAIForm));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.button_pause = new System.Windows.Forms.Button();
+            this.button_start = new System.Windows.Forms.Button();
+            this.waveformAiCtrl1 = new Automation.BDaq.WaveformAiCtrl(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chartXY = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartXY)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_stop
+            // 
+            this.button_stop.Location = new System.Drawing.Point(628, 450);
+            this.button_stop.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(76, 26);
+            this.button_stop.TabIndex = 19;
+            this.button_stop.Text = "Stop";
+            this.button_stop.UseVisualStyleBackColor = true;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
+            // 
+            // button_pause
+            // 
+            this.button_pause.Location = new System.Drawing.Point(544, 450);
+            this.button_pause.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_pause.Name = "button_pause";
+            this.button_pause.Size = new System.Drawing.Size(76, 26);
+            this.button_pause.TabIndex = 18;
+            this.button_pause.Text = "Pause";
+            this.button_pause.UseVisualStyleBackColor = true;
+            this.button_pause.Click += new System.EventHandler(this.button_pause_Click);
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(461, 450);
+            this.button_start.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(76, 26);
+            this.button_start.TabIndex = 17;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // waveformAiCtrl1
+            // 
+            this.waveformAiCtrl1._StateStream = ((Automation.BDaq.DeviceStateStreamer)(resources.GetObject("waveformAiCtrl1._StateStream")));
+            this.waveformAiCtrl1.DataReady += new System.EventHandler<Automation.BDaq.BfdAiEventArgs>(this.waveformAiCtrl1_DataReady);
+            this.waveformAiCtrl1.Overrun += new System.EventHandler<Automation.BDaq.BfdAiEventArgs>(this.waveformAiCtrl1_Overrun);
+            this.waveformAiCtrl1.CacheOverflow += new System.EventHandler<Automation.BDaq.BfdAiEventArgs>(this.waveformAiCtrl1_CacheOverflow);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(41, 450);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(2, 20);
+            this.label1.TabIndex = 20;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Location = new System.Drawing.Point(136, 452);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(2, 20);
+            this.label2.TabIndex = 21;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(266, 452);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(2, 20);
+            this.label3.TabIndex = 22;
+            // 
+            // chartXY
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartXY.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chartXY.Legends.Add(legend1);
+            this.chartXY.Location = new System.Drawing.Point(41, 12);
+            this.chartXY.Name = "chartXY";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartXY.Series.Add(series1);
+            this.chartXY.Size = new System.Drawing.Size(680, 410);
+            this.chartXY.TabIndex = 23;
+            this.chartXY.Text = "chart1";
+            // 
+            // StreamingAIForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 496);
+            this.Controls.Add(this.chartXY);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.button_pause);
+            this.Controls.Add(this.button_start);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "StreamingAIForm";
+            this.Text = "Streaming AI";
+            this.Load += new System.EventHandler(this.StreamingBufferedAiForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartXY)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+      }
+
+      #endregion
+      private System.Windows.Forms.Button button_stop;
+      private System.Windows.Forms.Button button_pause;
+      private System.Windows.Forms.Button button_start;
+		private Automation.BDaq.WaveformAiCtrl waveformAiCtrl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartXY;
+    }
+}
+
---
+++ AI_StreamingAI/StreamingAIForm.cs
@@ -0,0 +1,201 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Automation.BDaq;
+
+namespace AI_StreamingAI
+{
+   public partial class StreamingAIForm : Form
+   {
+      #region fields  
+
+      double[]     m_dataScaled;
+
+      bool         m_isFirstOverRun = true;
+      double       m_xInc;
+
+        string[] arrAvgData;
+        string[] arrData;
+        double[] arrSumData;
+        #endregion
+
+        public StreamingAIForm()
+      {
+         InitializeComponent();
+      }
+
+      public StreamingAIForm(int deviceNumber)
+      {
+         InitializeComponent();
+			waveformAiCtrl1.SelectedDevice = new DeviceInformation(deviceNumber);
+      }
+      
+      private void StreamingBufferedAiForm_Load(object sender, EventArgs e)
+      {
+         //The default device of project is demo device, users can choose other devices according to their needs. 
+		  if (!waveformAiCtrl1.Initialized)
+         {
+            MessageBox.Show("No device be selected or device open failed!", "StreamingAI");
+              this.Close();
+              return;
+         }
+
+			int chanCount = waveformAiCtrl1.Conversion.ChannelCount;
+			int sectionLength = waveformAiCtrl1.Record.SectionLength;
+			m_dataScaled = new double[chanCount * sectionLength];
+
+			this.Text = "Streaming AI(" + waveformAiCtrl1.SelectedDevice.Description + ")";
+
+         button_start.Enabled = true;
+         button_stop.Enabled = false;
+         button_pause.Enabled = false;
+      
+      }
+
+      private void HandleError(ErrorCode err)
+      {
+         if ((err >= ErrorCode.ErrorHandleNotValid) && (err != ErrorCode.Success))
+         {
+				MessageBox.Show("Sorry ! Some errors happened, the error code is: " + err.ToString(), "StreamingAI");
+         }
+      }
+
+      private void button_start_Click(object sender, EventArgs e)
+      {
+          ErrorCode err = ErrorCode.Success;
+
+			 err = waveformAiCtrl1.Prepare();
+          m_xInc = 1.0 / waveformAiCtrl1.Conversion.ClockRate;
+          if (err == ErrorCode.Success)
+          {
+				 err = waveformAiCtrl1.Start();
+                //waveformAiCtrl1.DataReady += new EventHandler<BfdAiEventArgs>(waveformAiCtrl1_DataReady);
+          }
+
+          if (err != ErrorCode.Success)
+          {
+				 HandleError(err);
+				 return;
+          }
+
+         button_start.Enabled = false;
+         button_pause.Enabled = true;
+         button_stop.Enabled = true;
+      }
+
+		private void waveformAiCtrl1_DataReady(object sender, BfdAiEventArgs args)
+      {
+            Console.WriteLine("halooooooooooooooooooooo");
+			try
+         {
+				//The WaveformAiCtrl has been disposed.
+				if (waveformAiCtrl1.State == ControlState.Idle)
+            {
+					return;
+            }
+            if (m_dataScaled.Length < args.Count)
+            {
+               m_dataScaled = new double[args.Count];
+            }
+
+            ErrorCode err = ErrorCode.Success;
+				int chanCount = waveformAiCtrl1.Conversion.ChannelCount;
+				int sectionLength = waveformAiCtrl1.Record.SectionLength;
+            err = waveformAiCtrl1.GetData(args.Count, m_dataScaled);
+
+            if (err != ErrorCode.Success && err != ErrorCode.WarningRecordEnd)
+            {
+               HandleError(err);
+               return;
+            }
+            System.Diagnostics.Debug.WriteLine(args.Count.ToString());
+
+                this.Invoke(new Action(() =>
+                {
+                    arrSumData = new double[chanCount];
+                    //listViewAi.BeginUpdate();
+
+                    for (int i = 0; i < sectionLength; i++)
+                    {
+                        arrData = new string[chanCount];
+                        for (int j = 0; j < chanCount; j++)
+                        {
+                            int cnt = i * chanCount + j;
+                            arrData[j] = m_dataScaled[cnt].ToString("F4");
+                            arrSumData[j] += m_dataScaled[cnt];
+                            Console.WriteLine("j ke " + j + " arrsumdata :" + arrSumData[j] + " m_datascaled: " + m_dataScaled[cnt] + " cnt: " + cnt + " chancount: " + chanCount);
+                        }
+                        //addListViewItems(listViewAi, arrData);
+                    }
+
+                    arrAvgData = new string[arrSumData.Length];
+
+                    for (int i = 0; i < arrSumData.Length; i++)
+                    {
+                        arrAvgData[i] = (arrSumData[i] / sectionLength).ToString("F4");
+                        label1.Text = arrAvgData[0];
+                        label2.Text = arrAvgData[1];
+                        label3.Text = arrAvgData[2];
+                        Console.WriteLine("i ke " + i + " arrsumdata :" + arrSumData[i]);
+                        chartXY.Series[0].Points.AddY(arrAvgData[0]);
+
+                    }
+                    //editListViewItems(listViewAi, 0, arrAvgData);
+
+                    //listViewAi.EndUpdate();
+                }));
+
+            }
+			catch (System.Exception) { }   
+       }
+
+      private void button_pause_Click(object sender, EventArgs e)
+      {
+         ErrorCode err = ErrorCode.Success;      
+			err = waveformAiCtrl1.Stop();
+         if (err != ErrorCode.Success)
+         {
+				HandleError(err);
+            return;
+         }
+
+         button_start.Enabled = true;
+         button_pause.Enabled = false;
+      }
+
+      private void button_stop_Click(object sender, EventArgs e)
+      {
+			ErrorCode err = ErrorCode.Success;
+			err = waveformAiCtrl1.Stop();
+         if (err != ErrorCode.Success)
+         {
+				HandleError(err);
+            return;
+         }   
+          
+         button_start.Enabled = true;
+         button_pause.Enabled = false;
+         button_stop.Enabled = false;
+         Array.Clear(m_dataScaled, 0, m_dataScaled.Length);
+         
+      }
+     
+		private void waveformAiCtrl1_CacheOverflow(object sender, BfdAiEventArgs e)
+      {
+         MessageBox.Show("WaveformAiCacheOverflow");
+      }
+
+        private void waveformAiCtrl1_Overrun(object sender, BfdAiEventArgs e)
+        {
+            if (m_isFirstOverRun)
+            {
+                MessageBox.Show("WaveformAiOverrun");
+                m_isFirstOverRun = false;
+            }
+        }
+    }
+}
