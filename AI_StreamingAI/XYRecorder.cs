cac9ef093804a2c729e48dafc85b5bcc6b960ccc
krishnayoga <ida.bagus52@ui.ac.id>
2018-08-19 18:03:19 +0700

    tambah credit

--- AI_StreamingAI/XYRecorder.cs
+++ AI_StreamingAI/XYRecorder.cs
@@ -1,3 +1,25 @@
+/*
+ * Dibuat Oleh:
+ * Ida Bagus Krishna Yoga Utama <email: hello@krishna.my.id >
+ * Arbariyanto Mahmud Wicaksono <email: arbariyantom@gmail.com>
+ * 
+ * Teknik Elektro 2015
+ * Departemen Teknik Elektro
+ * Universitas Indonesia
+ * 
+ * Dibuat pada Agustus 2018
+ * Untuk BPPT B2TKS Divisi SBPI
+*/
+
+/*
+ * Configuration waveformAICtrl1:
+ * Channel Count = 3;
+ * Frequency (Convert Clock Rate) = 8000;
+ * Section Length = 32;
+ * 
+ * Konfigurasi untuk sampling rate 10Hz (10 data per detik)
+*/
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
